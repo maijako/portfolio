60d368cecd05773ef76b595b1eeca69286e4069e
ensignjava <109529105+maijako@users.noreply.github.com>
2023-03-07 09:37:35 +0000

    import timeline and begin styling

--- portfolio-app/src/pages/Experience.js
+++ portfolio-app/src/pages/Experience.js
@@ -1,8 +1,23 @@
-import React from 'react'
+import React from 'react';
+import { VerticalTimeline, VerticalTimelineElement } from "react-vertical-timeline-component";
+import "react-vertical-timeline-component/style.min.css";
+
+
 
 function Experience() {
   return (
-    <div>Experience</div>
+    <div className="experience">
+      <VerticalTimeline lineColor="rgb(32, 32, 84)">
+        <VerticalTimelineElement className="vertical-timeline-element--education"
+        date="2023"
+        iconStyle={{ background: "rgb(32, 32, 84)", colo: "#fff"}}
+        
+        
+        >
+
+        </VerticalTimelineElement>
+      </VerticalTimeline>
+    </div>
   )
 }
 
