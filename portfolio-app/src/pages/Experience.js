cec2ea96420d8e1698355ef5512da80dc2a78406
ensignjava <109529105+maijako@users.noreply.github.com>
2023-03-07 14:28:02 +0000

    add app.css styling

--- portfolio-app/src/pages/Experience.js
+++ portfolio-app/src/pages/Experience.js
@@ -1,8 +1,9 @@
 import React from 'react';
 import { VerticalTimeline, VerticalTimelineElement } from "react-vertical-timeline-component";
 import "react-vertical-timeline-component/style.min.css";
-
-
+import "../styles/Experience.css";
+// import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+// import { solid } from '@fortawesome/fontawesome-svg-core/import.macro';
 
 function Experience() {
   return (
@@ -10,10 +11,10 @@ function Experience() {
       <VerticalTimeline lineColor="rgb(32, 32, 84)">
         <VerticalTimelineElement className="vertical-timeline-element--education"
         date="2023"
-        iconStyle={{ background: "rgb(32, 32, 84)", colo: "#fff"}}
-        
-        
+        iconStyle={{background: "rgb(32, 32, 84)"}}
         >
+          <h3 className="vertical-timeline-element-title">EdX, BootCamp</h3>
+          <p>Front-end Web Development Certificate</p>
 
         </VerticalTimelineElement>
       </VerticalTimeline>
