84d5f320f3f06fd79298c7c6e6f0de7d1d8e877b
ensignjava <109529105+maijako@users.noreply.github.com>
2023-03-07 16:22:58 +0000

    add projectItem

--- portfolio-app/src/pages/Projects.js
+++ portfolio-app/src/pages/Projects.js
@@ -1,15 +1,19 @@
 import React from 'react';
 import "../styles/Projects.css";
+import ProjectItem from "../components/ProjectItem";
+import Proj1 from "../assets/images/focus-pilot.PNG";
 
 function Projects() {
   return (
     <div className="wrapper projects">
       <h1>Projects</h1>
-      <div className="orjectList">
-        
+      <div className="ProjectList">
+        <ProjectItem name="Focus Pilot" image={Proj1}/>
+
+
       </div>
-      
-      </div>
+
+    </div>
   )
 }
 
